17dbf22488faf0ec5753161c1ac5e55fbe9a2fd1
k4ftt <103969132+k4ftt@users.noreply.github.com>
2022-07-07 17:10:42 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-li
+
 //════════════════════════════//
 //If you want to recode, reupload
 //or copy the codes/script,
@@ -37,10 +37,10 @@ global.APIKeys = {
 
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
